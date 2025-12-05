6ba139f3476b3045c669c194b6d60ff5dd669f41
msksirawich <sirawich@msyne.co.th>
2025-12-05 19:59:45 +0700

    update git ignore

--- target/run/medallion_project/models/example/my_second_dbt_model.sql
+++
@@ -1,9 +0,0 @@
-
-  
-  create view "medallion"."main"."my_second_dbt_model__dbt_tmp" as (
-    -- Use the `ref` function to select from other models
-
-select *
-from "medallion"."main"."my_first_dbt_model"
-where id = 1
-  );
